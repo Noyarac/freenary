dcf1303e2b83e1cfe7b15a7b1e567341495b556c
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-09-04 19:36:04 +0200

    ferst commit

---
+++ src/test/java/eu/carayon/freenary/FreenaryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.carayon.freenary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreenaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
